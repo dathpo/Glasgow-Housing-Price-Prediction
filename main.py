bf1c3a5ba7a7b8a4d454efebe23c64f0c494ff55
David <davidtpocock@gmail.com>
2018-11-01 18:55:33 +0000

    Add all other SIMD parameters

--- main.py
+++ main.py
@@ -50,42 +50,86 @@ def from_xls():
     return dz_df
 
 
+def merge_df(simd_df, left_df, column, year, first):
+    new_col = ''.join(column + '_' + str(year))
+    right_df = simd_df[['Datazone', new_col]]
+    right_df.columns = ['datazone', column]
+    right_df['sale_year'] = year
+    merged_df = pd.merge(left_df, right_df, how='left', on=['datazone', 'sale_year'])
+    dupl_col_x = ''.join(column + '_x')
+    dupl_col_y = ''.join(column + '_y')
+    if not first:
+        merged_df[column] = merged_df[dupl_col_x].fillna(merged_df[dupl_col_y])
+        merged_df.drop(columns=[dupl_col_x, dupl_col_y], inplace=True)
+    return merged_df
+
 dataframe = concatenate(prices_folder)
 datazone_df = from_xls()
+
 merged = pd.merge(dataframe, datazone_df, on='postcode')
 
-simd_df = pd.read_csv('data/simd/simd-overall-2004-2012-glasgow-v2.csv')
+overall_simd_df = pd.read_csv('data/simd/simd-overall-2004-2012-glasgow-v2.csv')
+employ_simd_df = pd.read_csv('data/simd/simd-employment-2004-2012-glasgow-v2.csv')
+income_simd_df = pd.read_csv('data/simd/simd-income-2004-2012-glasgow-v2.csv')
+health_simd_df = pd.read_csv('data/simd/simd-health-2004-2012-glasgow-v2.csv')
+edu_simd_df = pd.read_csv('data/simd/simd-education-2004-2012-glasgow-v2.csv')
+geo_simd_df = pd.read_csv('data/simd/simd-geographic-access-2004-2012-glasgow-v2.csv')
+housing_simd_df = pd.read_csv('data/simd/simd-housing-2004-2012-glasgow-v2.csv')
+crime_simd_df = pd.read_csv('data/simd/simd-crime-2006-2012-glasgow-v2.csv')
 
-simd_df_04 = simd_df[['Datazone', 'overall_deprivation_rank_2004']]
-simd_df_04.columns = ['datazone', 'overall_deprivation_rank']
-simd_df_04['sale_year'] = 2004
-merged_04 = pd.merge(merged, simd_df_04, how='left', on=['datazone', 'sale_year'])
+overall_col = 'overall_deprivation_rank'
+overall_04 = merge_df(overall_simd_df, merged, overall_col, 2004, True)
+overall_06 = merge_df(overall_simd_df, overall_04, overall_col, 2006, False)
+overall_09 = merge_df(overall_simd_df, overall_06, overall_col, 2009, False)
+overall_12 = merge_df(overall_simd_df, overall_09, overall_col, 2012, False)
 
-simd_df_06 = simd_df[['Datazone', 'overall_deprivation_rank_2006']]
-simd_df_06.columns = ['datazone', 'overall_deprivation_rank']
-simd_df_06['sale_year'] = 2006
-merged_06 = pd.merge(merged_04, simd_df_06, how='left', on=['datazone', 'sale_year'])
-merged_06['overall_deprivation_rank'] = merged_06['overall_deprivation_rank_x'].fillna(merged_06['overall_deprivation_rank_y'])
-merged_06.drop(columns=['overall_deprivation_rank_x', 'overall_deprivation_rank_y'], inplace=True)
+empl_col = 'employment_deprivation_rank'
+empl_04 = merge_df(employ_simd_df, overall_12, empl_col, 2004, True)
+empl_06 = merge_df(employ_simd_df, empl_04, empl_col, 2006, False)
+empl_09 = merge_df(employ_simd_df, empl_06, empl_col, 2009, False)
+empl_12 = merge_df(employ_simd_df, empl_09, empl_col, 2012, False)
 
-simd_df_09 = simd_df[['Datazone', 'overall_deprivation_rank_2009']]
-simd_df_09.columns = ['datazone', 'overall_deprivation_rank']
-simd_df_09['sale_year'] = 2009
-merged_09 = pd.merge(merged_06, simd_df_09, how='left', on=['datazone', 'sale_year'])
-merged_09['overall_deprivation_rank'] = merged_09['overall_deprivation_rank_x'].fillna(merged_09['overall_deprivation_rank_y'])
-merged_09.drop(columns=['overall_deprivation_rank_x', 'overall_deprivation_rank_y'], inplace=True)
+income_col = 'income_deprivation_rank'
+income_04 = merge_df(income_simd_df, empl_12, income_col, 2004, True)
+income_06 = merge_df(income_simd_df, income_04, income_col, 2006, False)
+income_09 = merge_df(income_simd_df, income_06, income_col, 2009, False)
+income_12 = merge_df(income_simd_df, income_09, income_col, 2012, False)
 
-simd_df_12 = simd_df[['Datazone', 'overall_deprivation_rank_2012']]
-simd_df_12.columns = ['datazone', 'overall_deprivation_rank']
-simd_df_12['sale_year'] = 2012
-merged_12 = pd.merge(merged_09, simd_df_12, how='left', on=['datazone', 'sale_year'])
-merged_12['overall_deprivation_rank'] = merged_12['overall_deprivation_rank_x'].fillna(merged_12['overall_deprivation_rank_y'])
-merged_12.drop(columns=['overall_deprivation_rank_x', 'overall_deprivation_rank_y'], inplace=True)
-merged_12 = merged_12.sort_values(by=['sale_date', 'july_2013_price'], ascending=False)
-merged_12 = merged_12[['street', 'postcode', 'datazone', 'local_housing_forum', 'overall_deprivation_rank',
-                       'sale_date', 'sale_quarter', 'sale_year', 'sale_month', 'nominal_price', 'july_2013_price', 'build', 'buyer_origin']]
-# merged_12.to_csv(path_or_buf='dataframe.csv', index=False)
-df = merged_12
+health_col = 'health_deprivation_rank'
+health_04 = merge_df(health_simd_df, income_12, health_col, 2004, True)
+health_06 = merge_df(health_simd_df, health_04, health_col, 2006, False)
+health_09 = merge_df(health_simd_df, health_06, health_col, 2009, False)
+health_12 = merge_df(health_simd_df, health_09, health_col, 2012, False)
+
+edu_col = 'education_deprivation_rank'
+edu_04 = merge_df(edu_simd_df, health_12, edu_col, 2004, True)
+edu_06 = merge_df(edu_simd_df, edu_04, edu_col, 2006, False)
+edu_09 = merge_df(edu_simd_df, edu_06, edu_col, 2009, False)
+edu_12 = merge_df(edu_simd_df, edu_09, edu_col, 2012, False)
+
+geo_col = 'geographic_access_deprivation_rank'
+geo_04 = merge_df(geo_simd_df, edu_12, geo_col, 2004, True)
+geo_06 = merge_df(geo_simd_df, geo_04, geo_col, 2006, False)
+geo_09 = merge_df(geo_simd_df, geo_06, geo_col, 2009, False)
+geo_12 = merge_df(geo_simd_df, geo_09, geo_col, 2012, False)
+
+housing_col = 'housing_deprivation_rank'
+housing_04 = merge_df(housing_simd_df, geo_12, housing_col, 2004, True)
+housing_06 = merge_df(housing_simd_df, housing_04, housing_col, 2006, False)
+housing_09 = merge_df(housing_simd_df, housing_06, housing_col, 2009, False)
+housing_12 = merge_df(housing_simd_df, housing_09, housing_col, 2012, False)
+
+crime_col = 'crime_deprivation_rank'
+crime_06 = merge_df(crime_simd_df, housing_12, crime_col, 2006, True)
+crime_09 = merge_df(crime_simd_df, crime_06, crime_col, 2009, False)
+crime_12 = merge_df(crime_simd_df, crime_09, crime_col, 2012, False)
+
+df = crime_12
+df = df.sort_values(by=['sale_date', 'july_2013_price'], ascending=False)
+df = df[['street', 'postcode', 'datazone', 'local_housing_forum', overall_col, empl_col, income_col,
+                     health_col, edu_col, geo_col, housing_col, crime_col, 'sale_date', 'sale_quarter',
+                     'sale_year', 'sale_month', 'nominal_price', 'july_2013_price', 'build', 'buyer_origin']]
+df.to_csv(path_or_buf='dataframe.csv', index=False)
 
 # rs_data = np.array(rs_fitness)
 # hs_internal = np.array(hc_internal_fitness)
